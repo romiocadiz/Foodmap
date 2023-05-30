0d7ff7fe91dc75755b4fb504d7478381ccfacac5
Janesse <129994325+itsjanesse@users.noreply.github.com>
2023-05-30 12:14:14 -0700

    updated files with buttons and pseudocode

---
+++ foodmap/foodmap/javascript/main.js
@@ -0,0 +1,31 @@
+//below will be the pseudocode I started
+
+//create a function that creates a filter menu that activates by clicking filter icon
+    //use checkboxes and input form for search bar
+        //put search bar on top right corner of form with placeholder "search"
+     //two categories of filter will be available:
+        //sort by: (1) distance (2) popularity (3) rating
+        //type of places: (1) cafes (2) dining halls (3) food trucks (4) markets
+    //there also needs to be an x on the input form to let user exit when clicked
+        //put x in top left corner 
+
+//create function that shows description of places
+    //content consists of name of place, hours, dining schedule, link to see full menu
+        //link will redirect user to another page for full menu: 
+            //breakfast, lunch, dinner, and late night
+
+//create an event listener for filter icon
+    //make the content of the function above show on click and disappear on clicking x
+    //make content sort by filters based off of options selected
+
+//create an event listener for search bar
+    //make the name of places appear when clicking on search bar
+    //submit search when search icon is pressed
+    //click elsewhere to exit search
+
+//create an event listener for icon of places
+    //make description of place pop up when icon is clicked
+    //make description go away when x icon is clicked
+
+//create an event listener for redirecting to menu
+        //add a link to redirect back to map on menu page
