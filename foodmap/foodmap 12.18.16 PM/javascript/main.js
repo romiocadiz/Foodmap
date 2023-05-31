c4c1a85c7e64c315b5af0816f071f48ab126a9ac
romiocadiz <rocadizc@ucsc.edu>
2023-05-30 19:30:42 -0700

    Nothing
    Search bar changes that were removed but still gotta push even though nothing has changed

--- foodmap/foodmap 12.18.16 PM/javascript/main.js
+++ foodmap/foodmap 12.18.16 PM/javascript/main.js
@@ -1,6 +1,6 @@
 //below will be the pseudocode I started
 
-//create a function that creates a filter menu that activates by clicking filter icon
+//(Sienna) create a function that creates a filter menu that activates by clicking filter icon
     //use checkboxes and input form for search bar
         //put search bar on top right corner of form with placeholder "search"
      //two categories of filter will be available:
@@ -9,6 +9,7 @@
     //there also needs to be an x on the input form to let user exit when clicked
         //put x in top left corner 
 
+<<<<<<< Updated upstream
 $(function() {
 
     $("#filterMenuButton").click(function() {
@@ -27,22 +28,26 @@ $(function() {
 });
 
 //create function that shows description of places
+=======
+//(Sienna) create function that shows description of places
+>>>>>>> Stashed changes
     //content consists of name of place, hours, dining schedule, link to see full menu
         //link will redirect user to another page for full menu: 
             //breakfast, lunch, dinner, and late night
 
-//create an event listener for filter icon
+//(Kali) create an event listener for filter icon
     //make the content of the function above show on click and disappear on clicking x
     //make content sort by filters based off of options selected
 
-//create an event listener for search bar
+//(Romi) create an event listener for search bar
     //make the name of places appear when clicking on search bar
     //submit search when search icon is pressed
     //click elsewhere to exit search
 
-//create an event listener for icon of places
+
+//(Tony) create an event listener for icon of places
     //make description of place pop up when icon is clicked
     //make description go away when x icon is clicked
 
-//create an event listener for redirecting to menu
+// (Ellie) create an event listener for redirecting to menu
         //add a link to redirect back to map on menu page
