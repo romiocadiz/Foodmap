adf03b4dd65ae7b676444acffce6f7b6d1b7bf05
Tony P <topau@ucsc.edu>
2023-06-13 22:05:00 -0700

    place 9. 11. 12
    9 11 12 done

--- javascript/main.js
+++ javascript/main.js
@@ -40,6 +40,27 @@ var locationDetails = {
     hours: "Mon - Thur: 8am - 8pm\nFri: 8am - 2pm",
     
   },
+  "place9": {
+    address: "414 McHenry Rd, Santa Cruz, CA 95064",
+    mapLink: "https://goo.gl/maps/EVq4BLngr3PUfpJN9",
+    menuLink: "https://nutrition.sa.ucsc.edu/shortmenu.aspx?sName=UC+Santa+Cruz+Dining&locationNum=46&locationName=Global+Village+Cafe&naFlag=1",
+    hours: "Mon - Thur: 8am - 8:30pm\nFri: 8am - 5pm",
+    
+  },
+  "place11": {
+    address: "520 Cowell-Stevenson Road, Santa Cruz, CA 95064",
+    mapLink: "https://goo.gl/maps/osGeBq2AqUc7tsk97",
+    menuLink: "https://nutrition.sa.ucsc.edu/shortmenu.aspx?sName=UC+Santa+Cruz+Dining&locationNum=05&locationName=Cowell%2fStevenson+Dining+Hall&naFlag=1",
+    hours: "Sun - Thur: 7am - 11pm\nFri - Sat: 7am - 8pm",
+    
+  },
+  "place12": {
+    address: "630 Crown Rd, Santa Cruz, CA 95064",
+    mapLink: "https://goo.gl/maps/f5Yxh6oT5qUAi37G7",
+    menuLink: "https://nutrition.sa.ucsc.edu/shortmenu.aspx?sName=UC+Santa+Cruz+Dining&locationNum=20&locationName=Crown%2fMerrill+Dining+Hall&naFlag=1",
+    hours: "Mon - Fri: 7am - 8pm",
+    
+  },
 
 }
 
